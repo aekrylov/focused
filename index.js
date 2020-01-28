35d03fc3659978d8dd6fc60fc89265b77fc41ea5
Anton Krylov <krylov@cg.ru>
2020-01-28 12:26:23 +0300

    add more rules, fix VK dynamic stuff

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ function del(elem) {
     elem.style.display = 'none';
 }
 
+let href = document.location.href;
+function add_observer(element, callback) {
+    const observer = new MutationObserver(mutations => {
+        if(mutations && document.location.href !== href) {
+            href = document.location.href;
+            callback();
+        }
+    });
+    observer.observe(element, {childList: true, attributes: true});
+}
+
 const rules = {
-    'habr.com': function f() {
+    'habr.com': function habr_com() {
         //сайдбары
         for (let sidebar of document.getElementsByClassName('sidebar')) {
             del(sidebar);
@@ -13,16 +24,29 @@ const rules = {
 
         //самое читаемое
         del(document.getElementById('broadcast_tabs_posts').parentElement.parentElement);
+
+        //лента
+        del(document.querySelector('.posts_list'));
     },
-    'vk.com': function () {
+    'vk.com/(im_)?feed': function vk_com() {
         //лента
         del(document.getElementById('main_feed'));
+
+        //реклама
+        del(document.getElementById('ads_left'));
     }
 };
 
-//execute all matching rules
-for (let rule in rules) {
-    if(document.location.href.match(rule)) {
-        rules[rule]();
+
+function apply_rules() {
+    //execute all matching rules
+    for (let rule in rules) {
+        if(document.location.href.match(rule)) {
+            rules[rule]();
+        }
     }
 }
+
+apply_rules();
+
+add_observer(document.body, apply_rules);
